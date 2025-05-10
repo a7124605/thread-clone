0e8cb1e88870602a4982e3c0a95603ba042a5bce
ChrisTsai <a7124605@gmail.com>
2025-05-10 22:13:36 +0800

    新增 Prettier 配置文件及相關依賴，並更新 ESLint 配置以整合 Prettier，調整 package.json 中的 lint 和 format 腳本，新增 .prettierignore 文件以排除不必要的檔案。

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -4,6 +4,6 @@
 // const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 // export const supabase = createClient(supabaseUrl, supabaseKey)
 
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
